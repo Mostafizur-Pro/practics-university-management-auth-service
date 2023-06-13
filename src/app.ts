c0cc4042a0a81b16fdef5aaed1dda700cb7fdb31
Mostafizur-Pro <mostafizur0195@gmail.com>
2023-06-13 16:02:05 +0600

    user router add

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,21 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+import { UserRoute } from './modules/user/user.route'
 
-const app: Application = express();
+const app: Application = express()
 
 // middleware
-app.use(cors());
+app.use(cors())
 
 // parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Working successfully");
-});
+// Application Routes
+app.use('/api/v1/users/', UserRoute)
 
-export default app;
+app.get('/', (req: Request, res: Response) => {
+  res.send('Working successfully')
+})
+
+export default app
--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,4 +1,10 @@
-// import express from "express"
+import express from 'express'
+import userController from './user.controller'
 
-// const router = express.Router()
-// Router.get('/', getUser)
+const router = express.Router()
+// create new user
+//
+router.post('/create-user', userController.createUser)
+// localhost: 5000 app.ts file a
+
+export const UserRoute = router
